b7cb05eb3c47615e3aa8f37466d6eff40c504fdf
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-12 20:48:35 -0400

    trakt configuration

---
+++ src/traktConfig.js
@@ -0,0 +1,10 @@
+export const traktConfig = {
+  user: "emrecaydin",
+  baseURL: "https://api.trakt.tv",
+  corsBase: "https://emre-origin.emree.workers.dev/corsproxy/?apiurl=",
+  headers: {
+    "Content-type": "application/json",
+    "trakt-api-key": "7ea1c22bef89c554327e0ad00ddba55de8b8cc7ae5c18cb8bb9c9dd8fdf47ea8",
+    "trakt-api-version": 2,
+  },
+};
