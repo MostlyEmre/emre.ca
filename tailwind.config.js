febb19086de324fb10af78c10d098b88fe7d9858
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-15 10:54:40 -0400

    Safelist update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    safelist: ["text-red-400", "text-blue-400", "text-yellow-400", "text-purple-400", "text-pink-400", "text-gray-900", "text-green-400"],
+    safelist: ["text-red-400", "text-blue-400", "text-yellow-400", "text-purple-400", "text-pink-400", "text-gray-900", "text-green-400", "hover:text-green-400"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
