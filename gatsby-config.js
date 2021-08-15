af18af94f98f274ae27a05fae35a16066f6d1f3c
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-15 10:36:08 -0400

    image plugins added

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,23 @@ module.exports = {
         path: `${__dirname}/src/about`,
       },
     },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-mdx`,
   ],
 };
