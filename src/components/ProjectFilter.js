17962860744368d98f0c820da03043578719442a
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-17 12:48:55 -0400

    Added Featured Projects section

---
+++ src/components/ProjectFilter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectFilter = () => {
+  return (
+    <div>
+      <p>this is a filter</p>
+      {/* Featured Projects */}
+      <button>Featured</button>
+    </div>
+  );
+};
+
+export default ProjectFilter;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,20 +4,35 @@ import { graphql, Link } from "gatsby";
 import { v4 as uuidv4 } from "uuid";
 import "../styles/global.css";
 import DevProjectCard from "../components/DevProjectCard";
+import ProjectFilter from "../components/ProjectFilter";
+
 const Projects = ({ data }) => {
   // const { title, stack, slug } = data.allMarkdownRemark.nodes.frontmatter;
   // console.log(data);
   const [projectFilter, setProjectFilter] = useState();
   console.log(data);
   const projects = data.allMarkdownRemark.nodes;
-
+  const featured = projects.filter((project) => project.frontmatter.featured);
+  console.log(featured);
   return (
     <Layout>
-      <h1 className="h2 my-5">Projects</h1>
-      <div className="flex flex-grow flex-wrap">
-        {projects.map((project) => (
-          <DevProjectCard key={uuidv4()} project={project} />
-        ))}
+      {/* Featured Projects */}
+      <div>
+        <h1 className="h2 my-5">Featured Projects</h1>
+        <div className="flex flex-grow flex-wrap">
+          {featured.map((project) => (
+            <DevProjectCard key={uuidv4()} project={project} />
+          ))}
+        </div>
+      </div>
+      <div>
+        <h1 className="h2 my-5">All Projects</h1>
+
+        <div className="flex flex-grow flex-wrap">
+          {projects.map((project) => (
+            <DevProjectCard key={uuidv4()} project={project} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
