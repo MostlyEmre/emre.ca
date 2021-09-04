c960349ade6e409e22be7a7a50185e119e4ac437
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-04 16:22:39 -0400

    Styling improvement for stack cards and general Project card.

--- src/components/SmallProjectCard.js
+++ src/components/SmallProjectCard.js
@@ -9,9 +9,9 @@ export const SmallProjectCard = ({ projectData }) => {
         <h3 className="text-2xl font-medium mb-2 cursor-default">{projectData.title}</h3>
 
         {/* PROJECT STACK */}
-        <ul className="flex flex-wrap cursor-default">
+        <ul className="flex flex-wrap mb-2 cursor-default">
           {projectData.stack.map((tech) => (
-            <li className="flex-grow bg-gray-300 mr-2 mb-2 px-2 py-1 text-xs text-center" key={uuidv4()}>
+            <li className="flex-grow bg-gray-300 text-sm text-gray-800 mr-1 mb-1 py-1 px-2 text-center" key={uuidv4()}>
               {tech}
             </li>
           ))}
@@ -21,17 +21,17 @@ export const SmallProjectCard = ({ projectData }) => {
       {projectData.live ? (
         <div>
           <div className="grid border-gray-300 grid-cols-2">
-            <a href={projectData.live} className="animation py-1 text-center text-sm text-gray-50 bg-purple-500 hover:bg-purple-700 font-bold uppercase" target="_blank" rel="noopener noreferrer">
+            <a href={projectData.live} className="animation py-2 text-center text-sm text-gray-50 bg-purple-500 hover:bg-purple-700 font-bold uppercase" target="_blank" rel="noopener noreferrer">
               Live Site
             </a>
-            <a href={projectData.source} className="animation text-center text-sm py-1 text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
+            <a href={projectData.source} className="animation text-center text-sm py-2 text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
               <button className="font-bold uppercase">Source Code</button>
             </a>
           </div>
         </div>
       ) : (
         <div className="grid border-t-4 border-gray-300 grid-cols-1">
-          <a href={projectData.source} className="animation py-1 text-center text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
+          <a href={projectData.source} className="animation py-2 text-center text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
             <button className="font-bold uppercase">Source Code</button>
           </a>
         </div>
