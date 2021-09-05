a2a6c19ca3d41b8652eb0ee1cef25df3c24dfc29
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-04 20:45:12 -0400

    Finalized styling for Featured project cards on home page

--- src/components/SmallProjectCard.js
+++ src/components/SmallProjectCard.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
-
+import { motion } from "framer-motion";
 export const SmallProjectCard = ({ projectData }) => {
   return (
-    <div className="p-4 flex flex-col justify-between bg-gray-100 max-w-md">
+    <div className="px-4 pt-3 pb-3 bg-gray-900 flex flex-col justify-between  max-w-md">
       {/* PROJECT TITLE */}
       <div>
-        <h3 className="text-2xl font-medium mb-2 cursor-default">{projectData.title}</h3>
+        <h3 className="text-2xl text-gray-50 font-medium mb-4 cursor-default">{projectData.title}</h3>
 
         {/* PROJECT STACK */}
         <ul className="flex flex-wrap mb-2 cursor-default">
           {projectData.stack.map((tech) => (
-            <li className="flex-grow bg-gray-300 text-sm text-gray-800 mr-2 mb-2 py-1 px-2 text-center" key={uuidv4()}>
+            <li className="flex-grow mr-2 bg-gray-700 text-sm text-gray-300 mb-2 py-1 px-2 text-center" key={uuidv4()}>
               {tech}
             </li>
           ))}
@@ -19,21 +19,42 @@ export const SmallProjectCard = ({ projectData }) => {
       </div>
       {/* PROJECT BUTTONS */}
       {projectData.live ? (
-        <div>
+        <div className="mb-1">
           <div className="grid border-gray-300 grid-cols-2">
-            <a href={projectData.live} className="animation py-2 text-center text-sm text-gray-50 bg-purple-500 hover:bg-purple-700 font-bold uppercase" target="_blank" rel="noopener noreferrer">
+            <motion.a
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.9 }}
+              href={projectData.live}
+              className="bg-pink-600 py-2 text-center text-sm text-gray-50 hover:bg-pink-800 font-bold uppercase"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Live Site
-            </a>
-            <a href={projectData.source} className="animation text-center text-sm py-2 text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.9 }}
+              href={projectData.source}
+              className="hover:text-gray-300 text-center text-sm py-2 text-gray-50"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="font-bold uppercase">Source Code</button>
-            </a>
+            </motion.a>
           </div>
         </div>
       ) : (
         <div className="grid border-t-4 border-gray-300 grid-cols-1">
-          <a href={projectData.source} className="animation py-2 text-center text-gray-900 bg-gray-50 hover:bg-gray-200" target="_blank" rel="noopener noreferrer">
+          <motion.a
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.9 }}
+            href={projectData.source}
+            className="hover:text-gray-300 text-center text-sm py-2 text-gray-50"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button className="font-bold uppercase">Source Code</button>
-          </a>
+          </motion.a>
         </div>
       )}
     </div>
