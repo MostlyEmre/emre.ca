91d48cf3db67b2d36ef0cdb912241f4bc1ba55f9
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-06 23:22:29 -0400

    Updated header color

--- src/components/HeaderCard.js
+++ src/components/HeaderCard.js
@@ -7,7 +7,7 @@ const HeaderCard = () => {
     <div className="max-w-md">
       <motion.div whileHover={{ scale: 1.015 }} whileTap={{ scale: 0.95 }}>
         <Link whileHover={{ scale: 1.015 }} whileTap={{ scale: 0.95 }} to="/">
-          <p className="font-bold text-4xl sm:text-5xl cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-purple-400">
+          <p className="font-extrabold text-4xl sm:text-5xl cursor-pointer text-transparent bg-clip-text bg-gradient-to-tr from-gray-500 via-gray-900 to-black">
             Hello, I'm Emre!
             <sup className="cursor-default">
               <span className="font-normal text-sm sm:text-md ml-2 my-2 text-gray-600">/Am + Rae/</span>
