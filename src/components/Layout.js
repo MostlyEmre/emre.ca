116ca0738c1c29820e43cb1d7d23ef035c63042c
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-10-14 19:36:14 -0400

    Centered the website

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="w-11/12 m-auto text-gray-900">
+    <div className="w-11/12 md:w-6/12 m-auto text-gray-900">
       <Header />
       {children}
       <Footer />
