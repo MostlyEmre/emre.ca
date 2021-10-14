4604e2042366975b5c91075e9f35de57894997e9
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-10-14 19:35:59 -0400

    Removed Social Card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ import Social from "./Social";
 const Footer = () => {
   return (
     <div className="max-w-md border-t-2 pt-5 mb-5">
-      <p className="font-light text-gray-400 cursor-default">Designed and developed by Emre.</p>
-      <Social marginBottom={`mb-4`} marginTop={`mt-4`} />
+      <p className="font-light text-gray-400 cursor-default">Designed and developed by me.</p>
     </div>
   );
 };
